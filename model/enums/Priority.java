daa838772e46b27a11eb06e02a19b98926c3d2d1
shi101-hash <135109233+shi101-hash@users.noreply.github.com>
2025-08-07 21:53:50 +0530

    Add files via upload

---
+++ model/enums/Priority.java
@@ -0,0 +1,7 @@
+package com.example.workforce.model.enums;
+
+public enum Priority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ model/enums/ReferenceType.java
@@ -0,0 +1,7 @@
+package com.example.workforce.model.enums;
+
+public enum ReferenceType {
+    ENTITY,   // Customer / Transporter
+    ORDER,
+    ENQUIRY
+}
